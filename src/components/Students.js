b93c6c91057f1bfaa09b3fbde380a471c1e7a53e
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-05-10 11:42:39 +0200

    Solutions with the errors in PUT and POST requests

---
+++ src/components/Students.js
@@ -0,0 +1,45 @@
+const Students = () => {
+  return (
+    <div className='container'>
+      <h1>Students</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Year of Birth</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Year of Birth</td>
+            <td>Address</td>
+          </tr>
+          <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Year of Birth</td>
+            <td>Address</td>
+          </tr>
+          <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Year of Birth</td>
+            <td>Address</td>
+          </tr>
+          <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Year of Birth</td>
+            <td>Address</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Students;
